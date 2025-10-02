3ad62378ba5e6385369347b662c8c32999ce2871
matty <mattreed1890@gmail.com>
2025-10-02 14:22:03 -0500

    linux

--- src/bulkin.cpp
+++ src/bulkin.cpp
@@ -18,43 +18,51 @@ void Bulkin::initWindow() {
   glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 }
 
-void Bulkin::framebufferResizeCallback(GLFWwindow *window, int width, int height) {
-  auto app = reinterpret_cast<Bulkin*>(glfwGetWindowUserPointer(window));
+void Bulkin::framebufferResizeCallback(GLFWwindow *window, int width,
+                                       int height) {
+  auto app = reinterpret_cast<Bulkin *>(glfwGetWindowUserPointer(window));
   app->framebufferResized = true;
 }
 
 void Bulkin::mouse_callback(GLFWwindow *window, double x, double y) {
-  auto app = reinterpret_cast<Bulkin*>(glfwGetWindowUserPointer(window));
+  auto app = reinterpret_cast<Bulkin *>(glfwGetWindowUserPointer(window));
   int width, height;
   glfwGetFramebufferSize(window, &width, &height);
   float xpos = static_cast<float>(x / width);
   float ypos = static_cast<float>(y / height);
-  
+
   if (app->firstMouse) {
     app->lastX = xpos;
     app->lastY = ypos;
     app->firstMouse = false;
   }
-  
+
   float xoffset = xpos - app->lastX;
   float yoffset = app->lastY - ypos;
-  
+
   app->lastX = xpos;
   app->lastY = ypos;
-  
+
   app->mouseState.pos.x = xoffset;
   app->mouseState.pos.y = yoffset;
 }
 
-void Bulkin::key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
-  auto app = reinterpret_cast<Bulkin*>(glfwGetWindowUserPointer(window));
+void Bulkin::key_callback(GLFWwindow *window, int key, int scancode, int action,
+                          int mods) {
+  auto app = reinterpret_cast<Bulkin *>(glfwGetWindowUserPointer(window));
   const bool press = action != GLFW_RELEASE;
-  if (key == GLFW_KEY_ESCAPE) glfwSetWindowShouldClose(window, GLFW_TRUE);
-  if (key == GLFW_KEY_W) app->camera.movement.forward = press;
-  if (key == GLFW_KEY_S) app->camera.movement.backward = press;
-  if (key == GLFW_KEY_A) app->camera.movement.left = press;
-  if (key == GLFW_KEY_D) app->camera.movement.right = press;
-  if (mods & GLFW_MOD_SHIFT) app->camera.movement.fast = press;
+  if (key == GLFW_KEY_ESCAPE)
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
+  if (key == GLFW_KEY_W)
+    app->camera.movement.forward = press;
+  if (key == GLFW_KEY_S)
+    app->camera.movement.backward = press;
+  if (key == GLFW_KEY_A)
+    app->camera.movement.left = press;
+  if (key == GLFW_KEY_D)
+    app->camera.movement.right = press;
+  if (mods & GLFW_MOD_SHIFT)
+    app->camera.movement.fast = press;
 }
 
 void Bulkin::initVulkan() {
@@ -72,72 +80,81 @@ void Bulkin::mainLoop() {
     glfwPollEvents();
     drawFrame();
   }
-  
+
   device.device.waitIdle();
 }
 
 void Bulkin::drawFrame() {
-  if(device.device.waitForFences(1, &drawFences[currentFrame], vk::True, UINT64_MAX) != vk::Result::eSuccess)
+  if (device.device.waitForFences(1, &drawFences[currentFrame], vk::True,
+                                  UINT64_MAX) != vk::Result::eSuccess)
     throw std::runtime_error("failed to wait for fence");
-  
+
   uint32_t imageIndex;
-  auto result = device.device.acquireNextImageKHR(device.swapchain.swapchain,
-                                                  UINT64_MAX,
-                                                  presentCompleteSemaphores[currentFrame],
-                                                  nullptr,
-                                                  &imageIndex);
-  
+  auto result = device.device.acquireNextImageKHR(
+      device.swapchain.swapchain, UINT64_MAX,
+      presentCompleteSemaphores[currentFrame], nullptr, &imageIndex);
+
   if (result == vk::Result::eErrorOutOfDateKHR) {
-    device.swapchain.recreate(device.device, device.surface, window, device.findQueueFamilies(device.physicalDevice));
-  } else if (result != vk::Result::eSuccess && result != vk::Result::eSuboptimalKHR) {
+    device.swapchain.recreate(device.device, device.surface, window,
+                              device.findQueueFamilies(device.physicalDevice));
+  } else if (result != vk::Result::eSuccess &&
+             result != vk::Result::eSuboptimalKHR) {
     throw std::runtime_error("failed to acquire next image");
   }
-  
-  if(device.device.resetFences(1, &drawFences[currentFrame]) != vk::Result::eSuccess)
+
+  if (device.device.resetFences(1, &drawFences[currentFrame]) !=
+      vk::Result::eSuccess)
     throw std::runtime_error("failed to reset fence");
-  
+
   update();
-  
+
   device.graphicsPipeline.commandBuffers[currentFrame].reset();
-  device.graphicsPipeline.recordCommandBuffer(device.graphicsPipeline.commandBuffers[currentFrame], imageIndex, device.swapchain, currentFrame, quad);
-  
+  device.graphicsPipeline.recordCommandBuffer(
+      device.graphicsPipeline.commandBuffers[currentFrame], imageIndex,
+      device.swapchain, currentFrame, quad);
+
   vk::SubmitInfo submitInfo{};
-  
+
   vk::Semaphore waitSemaphores[] = {presentCompleteSemaphores[currentFrame]};
-  vk::PipelineStageFlags waitStages[] = {vk::PipelineStageFlagBits::eColorAttachmentOutput};
-  
+  vk::PipelineStageFlags waitStages[] = {
+      vk::PipelineStageFlagBits::eColorAttachmentOutput};
+
   submitInfo.waitSemaphoreCount = 1;
   submitInfo.pWaitSemaphores = waitSemaphores;
   submitInfo.pWaitDstStageMask = waitStages;
-  
+
   submitInfo.commandBufferCount = 1;
-  submitInfo.pCommandBuffers = &device.graphicsPipeline.commandBuffers[currentFrame];
-  
+  submitInfo.pCommandBuffers =
+      &device.graphicsPipeline.commandBuffers[currentFrame];
+
   vk::Semaphore signalSemaphores[] = {renderFinishedSemaphores[imageIndex]};
   submitInfo.signalSemaphoreCount = 1;
   submitInfo.pSignalSemaphores = signalSemaphores;
-  
-  if (device.graphicsQueue.submit(1, &submitInfo, drawFences[currentFrame]) != vk::Result::eSuccess)
+
+  if (device.graphicsQueue.submit(1, &submitInfo, drawFences[currentFrame]) !=
+      vk::Result::eSuccess)
     throw std::runtime_error("failed to submit graphics queue");
-  
+
   vk::PresentInfoKHR presentInfo{};
   presentInfo.waitSemaphoreCount = 1;
   presentInfo.pWaitSemaphores = signalSemaphores;
-  
+
   vk::SwapchainKHR swapChains[] = {device.swapchain.swapchain};
   presentInfo.swapchainCount = 1;
   presentInfo.pSwapchains = swapChains;
   presentInfo.pImageIndices = &imageIndex;
-  
+
   result = device.presentQueue.presentKHR(presentInfo);
-  
-  if (result == vk::Result::eErrorOutOfDateKHR || result == vk::Result::eSuboptimalKHR || framebufferResized) {
+
+  if (result == vk::Result::eErrorOutOfDateKHR ||
+      result == vk::Result::eSuboptimalKHR || framebufferResized) {
     framebufferResized = false;
-    device.swapchain.recreate(device.device, device.surface, window, device.findQueueFamilies(device.physicalDevice));
+    device.swapchain.recreate(device.device, device.surface, window,
+                              device.findQueueFamilies(device.physicalDevice));
   } else if (result != vk::Result::eSuccess) {
     throw std::runtime_error("failed to present graphics queue");
   }
-  
+
   currentFrame = (currentFrame + 1) % MAX_FRAMES_IN_FLIGHT;
 }
 
@@ -147,20 +164,24 @@ void Bulkin::update() {
   timeStamp = newTimeStamp;
   camera.update(deltaTime, mouseState.pos);
   updatePushConstants();
-  
-  device.graphicsPipeline.buffers.updateUniformBuffer(currentFrame, static_cast<float>(device.swapchain.extent.width), static_cast<float>(device.swapchain.extent.height), camera);
+
+  device.graphicsPipeline.buffers.updateUniformBuffer(
+      currentFrame, static_cast<float>(device.swapchain.extent.width),
+      static_cast<float>(device.swapchain.extent.height), camera);
 }
 
 void Bulkin::updatePushConstants() {
-//  glm::mat4 model(1.0f);
-//  
-//  model = glm::translate(model, glm::vec3(0.0f));
-//  model = glm::rotate(model, glm::radians(0.0f), glm::vec3(1.0f));
-//  model = glm::scale(model, glm::vec3(1.0f));
-//  
-//  pushConstants[currentFrame].model = model;
-//  pushConstants[currentFrame].view = camera.getView();
-//  pushConstants[currentFrame].proj = glm::perspective(glm::radians(45.0f), device.swapchain.extent.width / (float) device.swapchain.extent.height, 0.1f, 100.0f);
+  //  glm::mat4 model(1.0f);
+  //
+  //  model = glm::translate(model, glm::vec3(0.0f));
+  //  model = glm::rotate(model, glm::radians(0.0f), glm::vec3(1.0f));
+  //  model = glm::scale(model, glm::vec3(1.0f));
+  //
+  //  pushConstants[currentFrame].model = model;
+  //  pushConstants[currentFrame].view = camera.getView();
+  //  pushConstants[currentFrame].proj = glm::perspective(glm::radians(45.0f),
+  //  device.swapchain.extent.width / (float) device.swapchain.extent.height,
+  //  0.1f, 100.0f);
 }
 
 void Bulkin::cleanup() {
@@ -184,30 +205,32 @@ void Bulkin::createInstance() {
   appInfo.pEngineName = "bulkin engine";
   appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
   appInfo.apiVersion = VK_API_VERSION_1_3;
-  
+
   vk::InstanceCreateInfo createInfo{};
   createInfo.pApplicationInfo = &appInfo;
-  
+
   uint32_t glfwExtensionCount = 0;
-  const char** glfwExtensions;
-  
+  const char **glfwExtensions;
+
   glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-  
-  std::vector<const char*> requiredExtensions;
-  
+
+  std::vector<const char *> requiredExtensions;
+
   for (uint32_t i = 0; i < glfwExtensionCount; i++)
     requiredExtensions.emplace_back(glfwExtensions[i]);
-  
+
   requiredExtensions.emplace_back(vk::KHRPortabilityEnumerationExtensionName);
-  
+
   createInfo.flags |= vk::InstanceCreateFlagBits::eEnumeratePortabilityKHR;
-  
-  createInfo.enabledExtensionCount = static_cast<uint32_t>(requiredExtensions.size());
+
+  createInfo.enabledExtensionCount =
+      static_cast<uint32_t>(requiredExtensions.size());
   createInfo.ppEnabledExtensionNames = requiredExtensions.data();
-  
+
   createInfo.enabledLayerCount = 0;
-  
-  if (vk::createInstance(&createInfo, nullptr, &instance) != vk::Result::eSuccess)
+
+  if (vk::createInstance(&createInfo, nullptr, &instance) !=
+      vk::Result::eSuccess)
     throw std::runtime_error("failed to create instance");
 }
 
@@ -216,22 +239,23 @@ void Bulkin::createSyncObjects() {
   presentCompleteSemaphores.resize(MAX_FRAMES_IN_FLIGHT);
   renderFinishedSemaphores.resize(imageSize);
   drawFences.resize(MAX_FRAMES_IN_FLIGHT);
-  
+
   for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
-    presentCompleteSemaphores[i] = device.device.createSemaphore(vk::SemaphoreCreateInfo());
+    presentCompleteSemaphores[i] =
+        device.device.createSemaphore(vk::SemaphoreCreateInfo());
     vk::FenceCreateInfo fenceInfo{};
     fenceInfo.flags = vk::FenceCreateFlagBits::eSignaled;
     drawFences[i] = device.device.createFence(fenceInfo);
   }
   for (size_t i = 0; i < imageSize; i++) {
-    renderFinishedSemaphores[i] = device.device.createSemaphore(vk::SemaphoreCreateInfo());
+    renderFinishedSemaphores[i] =
+        device.device.createSemaphore(vk::SemaphoreCreateInfo());
   }
 }
 
-void Bulkin::addQuad(glm::vec3 position, float rotationX, float rotationY, float rotationZ, float scale) {
+void Bulkin::addQuad(glm::vec3 position, float rotationX, float rotationY,
+                     float rotationZ, float scale) {
   quad.addQuad(position, rotationX, rotationY, rotationZ, scale);
 }
 
-void Bulkin::setPlayerPos(glm::vec2 pos) {
-  camera.setPlayerPos(pos);
-}
+void Bulkin::setPlayerPos(glm::vec2 pos) { camera.setPlayerPos(pos); }
--- src/device.cpp
+++ src/device.cpp
@@ -2,54 +2,62 @@
 
 #include <set>
 
-std::vector<const char*> requiredExtensions{"VK_KHR_portability_subset", vk::KHRSwapchainExtensionName, "VK_KHR_shader_draw_parameters", vk::KHRSynchronization2ExtensionName};
+#ifdef __APPLE__
+std::vector<const char *> requiredExtensions{
+    "VK_KHR_portability_subset", vk::KHRSwapchainExtensionName,
+    "VK_KHR_shader_draw_parameters", vk::KHRSynchronization2ExtensionName};
+#else
+std::vector<const char *> requiredExtensions{
+    /*"VK_KHR_portability_subset", */ vk::KHRSwapchainExtensionName,
+    "VK_KHR_shader_draw_parameters", vk::KHRSynchronization2ExtensionName};
+#endif // __APPLE__
 
-QueueFamilyIndices BulkinDevice::findQueueFamilies(vk::PhysicalDevice& device) {
+QueueFamilyIndices BulkinDevice::findQueueFamilies(vk::PhysicalDevice &device) {
   QueueFamilyIndices indices;
-  
+
   auto queueFamilies = device.getQueueFamilyProperties();
-  
+
   int i = 0;
-  for (const auto& queueFamily : queueFamilies) {
+  for (const auto &queueFamily : queueFamilies) {
     if (queueFamily.queueFlags & vk::QueueFlagBits::eGraphics) {
       indices.graphicsFamily = i;
     }
-    
+
     auto presentSupport = device.getSurfaceSupportKHR(i, surface);
-    
+
     if (presentSupport)
       indices.presentFamily = i;
-    
+
     if (indices.isComplete())
       break;
-    
+
     i++;
   }
-  
+
   return indices;
 }
 
-void BulkinDevice::pickPhysicalDevice(vk::Instance& instance) {
+void BulkinDevice::pickPhysicalDevice(vk::Instance &instance) {
   auto devices = instance.enumeratePhysicalDevices();
   if (devices.empty())
     throw std::runtime_error("failed to find GPUs with vulkan support");
-    
-  for (auto& device : devices) {
+
+  for (auto &device : devices) {
     if (isDeviceSuitable(device)) {
       physicalDevice = device;
       break;
     }
   }
-  
+
   if (!physicalDevice)
     throw std::runtime_error("failed to find suitable GPU");
 }
 
-bool BulkinDevice::isDeviceSuitable(vk::PhysicalDevice& physicalDevice) {
+bool BulkinDevice::isDeviceSuitable(vk::PhysicalDevice &physicalDevice) {
   auto indices = findQueueFamilies(physicalDevice);
-  
+
   bool extensionsSupported = checkDeviceExtensionSupport(physicalDevice);
-  
+
   bool swapchainAdequate = false;
   if (extensionsSupported) {
     BulkinSwapchain swapchain;
@@ -61,33 +69,36 @@ bool BulkinDevice::isDeviceSuitable(vk::PhysicalDevice& physicalDevice) {
   return indices.isComplete() && extensionsSupported && swapchainAdequate;
 }
 
-bool BulkinDevice::checkDeviceExtensionSupport(vk::PhysicalDevice& physicalDevice) {
-  auto availableExtensions = physicalDevice.enumerateDeviceExtensionProperties();
-  
-  for (const auto& extension : requiredExtensions) {
+bool BulkinDevice::checkDeviceExtensionSupport(
+    vk::PhysicalDevice &physicalDevice) {
+  auto availableExtensions =
+      physicalDevice.enumerateDeviceExtensionProperties();
+
+  for (const auto &extension : requiredExtensions) {
     bool extensionFound = false;
-    
-    for (const auto& ext : availableExtensions) {
+
+    for (const auto &ext : availableExtensions) {
       if (strcmp(extension, ext.extensionName) == 0) {
         extensionFound = true;
         break;
       }
     }
-    
+
     if (!extensionFound) {
       return false;
     }
   }
-  
+
   return true;
 }
 
 void BulkinDevice::createLogicalDevice() {
   auto indices = findQueueFamilies(physicalDevice);
-  
+
   std::vector<vk::DeviceQueueCreateInfo> queueCreateInfos{};
-  std::set<uint32_t> uniqueQueueFamilies = {indices.graphicsFamily.value(), indices.presentFamily.value()};
-  
+  std::set<uint32_t> uniqueQueueFamilies = {indices.graphicsFamily.value(),
+                                            indices.presentFamily.value()};
+
   float queuePriority = 1.0f;
   for (uint32_t queueFamily : uniqueQueueFamilies) {
     vk::DeviceQueueCreateInfo queueCreateInfo{};
@@ -96,35 +107,41 @@ void BulkinDevice::createLogicalDevice() {
     queueCreateInfo.pQueuePriorities = &queuePriority;
     queueCreateInfos.push_back(queueCreateInfo);
   }
-    
+
   vk::PhysicalDeviceFeatures2 deviceFeatures;
   vk::PhysicalDeviceVulkan13Features vulkan13Features;
   vulkan13Features.dynamicRendering = true;
   vulkan13Features.synchronization2 = true;
   vk::PhysicalDeviceExtendedDynamicStateFeaturesEXT extendedStateFeatures;
   extendedStateFeatures.extendedDynamicState = true;
-  vk::StructureChain<vk::PhysicalDeviceFeatures2, vk::PhysicalDeviceVulkan13Features, vk::PhysicalDeviceExtendedDynamicStateFeaturesEXT> featureChain = {
-      deviceFeatures,         // vk::PhysicalDeviceFeatures2 (empty for now)
-      vulkan13Features,       // Enable dynamic rendering from Vulkan 1.3
-      extendedStateFeatures   // Enable extended dynamic state from the extension
-  };
-  
+  vk::StructureChain<vk::PhysicalDeviceFeatures2,
+                     vk::PhysicalDeviceVulkan13Features,
+                     vk::PhysicalDeviceExtendedDynamicStateFeaturesEXT>
+      featureChain = {
+          deviceFeatures,       // vk::PhysicalDeviceFeatures2 (empty for now)
+          vulkan13Features,     // Enable dynamic rendering from Vulkan 1.3
+          extendedStateFeatures // Enable extended dynamic state from the
+                                // extension
+      };
+
   vk::DeviceCreateInfo createInfo{};
   createInfo.pNext = &featureChain.get<vk::PhysicalDeviceFeatures2>();
   createInfo.pQueueCreateInfos = queueCreateInfos.data();
-  createInfo.queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos.size());
-  
-  createInfo.enabledExtensionCount = static_cast<uint32_t>(requiredExtensions.size());
+  createInfo.queueCreateInfoCount =
+      static_cast<uint32_t>(queueCreateInfos.size());
+
+  createInfo.enabledExtensionCount =
+      static_cast<uint32_t>(requiredExtensions.size());
   createInfo.ppEnabledExtensionNames = requiredExtensions.data();
   createInfo.enabledLayerCount = 0;
-  
+
   device = physicalDevice.createDevice(createInfo);
-  
+
   device.getQueue(indices.graphicsFamily.value(), 0, &graphicsQueue);
   device.getQueue(indices.presentFamily.value(), 0, &presentQueue);
 }
 
-void BulkinDevice::cleanup(vk::Instance& instance) {
+void BulkinDevice::cleanup(vk::Instance &instance) {
   graphicsPipeline.cleanup(device);
   swapchain.cleanup(device);
   device.destroy();
@@ -133,14 +150,16 @@ void BulkinDevice::cleanup(vk::Instance& instance) {
 
 void BulkinDevice::createSurface(vk::Instance &instance, GLFWwindow *window) {
   VkSurfaceKHR _surface;
-  if (glfwCreateWindowSurface(instance, window, nullptr, &_surface) != VK_SUCCESS)
+  if (glfwCreateWindowSurface(instance, window, nullptr, &_surface) !=
+      VK_SUCCESS)
     throw std::runtime_error("failed to create window surface");
-  
+
   surface = vk::SurfaceKHR(_surface);
 }
 
 void BulkinDevice::createSwapchain(GLFWwindow *window) {
-  swapchain.createSwapchain(device, surface, window, findQueueFamilies(physicalDevice));
+  swapchain.createSwapchain(device, surface, window,
+                            findQueueFamilies(physicalDevice));
   swapchain.createImageViews(device);
 }
 
