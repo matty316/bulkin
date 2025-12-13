5e2b70dc024a3b4115c0c3b8394ff0af09f6b4b5
matty <mattreed1890@gmail.com>
2025-12-13 13:51:38 -0600

    Update bulkin-pipelines.hpp

--- include/bulkin-pipelines.hpp
+++ include/bulkin-pipelines.hpp
@@ -1,6 +1,13 @@
 #pragma once
 
 #include <vulkan/vulkan.h>
+#include <vector>
+
+class BulkinPipeline {
+public:
+  std::vector<VkPipelineShaderStageCreateInfo> shader_stages;
+
+};
 
 namespace vkutil {
   bool load_shader_module(const char* file_path, VkDevice device, VkShaderModule *out_module);
